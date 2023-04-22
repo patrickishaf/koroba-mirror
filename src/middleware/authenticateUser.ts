37292afd04a141d04fc79cdc01bd70cd259721a3
oscarnovember <ndukwe.peter@roqqu.com>
2023-04-23 00:19:32 +0100

    rewrite middleware for authenticating jwt

--- src/middleware/authenticateUser.ts
+++ src/middleware/authenticateUser.ts
@@ -1,16 +1,19 @@
-import { type NextFunction, type Request, type Response } from "express";
+import { type NextFunction, type Response } from "express";
 import { verify } from "jsonwebtoken"
 import { ErrorResponse } from "../net";
 import * as ErrorMessages from "../net/errorMessages";
 
-const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
+const authenticateUser = (req, res: Response, next: NextFunction) => {
   const { authorization } = req.headers
-  if (authorization === undefined) {
-    return res.status(401).json(new ErrorResponse(ErrorMessages.unauthorized))
-  }
   const token = authorization && authorization.split(' ')[1]
-  const isValid = verify(token)
-  next();
+  if (token === null) {
+    return res.status(401).json(new ErrorResponse(ErrorMessages.unauthorized));
+  } 
+  verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json(new ErrorResponse(ErrorMessages.invalidToken));
+    req.user = user;
+    next();
+  })
 }
 
 export default authenticateUser;

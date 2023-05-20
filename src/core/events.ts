9e058ca99ce0cec892eddfef7e517dd11b242aab
oscarnovember <ndukwe.peter@roqqu.com>
2023-05-20 16:23:32 +0100

    create trading events

--- src/core/events.ts
+++ src/core/events.ts
@@ -1,3 +1,20 @@
-export const SettingsEvents = {
-  CREATE_USER_SETTINGS: 'create_user_settings'
+export enum SettingsEvents {
+  CREATE_USER_SETTINGS = 'create_user_settings'
 };
+
+export enum TradingEvents {
+  SEND_COIN_REQUESTED = 'send_coin_requested',
+  BUY_COIN_REQUESTED = 'buy_coin_requested',
+  SELL_COIN_REQUESTED = 'sell_coin_requested',
+  SWAP_COIN_REQUESTED = 'swap_coin_requested',
+  CONVERSION_SUCCESS = 'conversion_success',
+  CONVERSION_FAILURE = 'conversion_failure',
+  DEDUCTION_SUCCESS = 'deduction_success',
+  DEDUCTION_FAILURE = 'deduction_failure',
+  TOPUP_SUCCESS = 'topup_success',
+  TOPUP_FAILURE = 'topup_failure',
+  REVERSAL_SUCCESS = 'reversal_success',
+  REVERSAL_FAILURE = 'reversal_failure',
+  TRANSACTION_SAVE_SUCCESS = 'txn_save_success',
+  TRANSACTION_SAVE_FAILURE = 'txn_save_failure'
+}

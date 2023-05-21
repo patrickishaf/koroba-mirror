ec3b75d402dd5b26a1d42da6be357266bdaf8e01
oscarnovember <ndukwe.peter@roqqu.com>
2023-05-21 11:12:57 +0100

    modularize validation functions

---
+++ src/validation/index.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+import { ErrorResponse } from "../net";
+
+export const useSchemaToValidate = (schema: Joi.ObjectSchema<any>, req: Request, res: Response, next: NextFunction) => {
+  const validationState = schema.validate(req.body);
+  if (validationState.hasOwnProperty('error')) {
+    return res.status(400).send(createErrorResponseFromValidationState(validationState));
+  }
+  next();
+}
+
+const createErrorResponseFromValidationState = (validationState: any) => {
+  return ErrorResponse.from(validationState.error.details[0].message);
+}

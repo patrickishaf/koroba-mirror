831f8114fc7802d70bf1928567acca666de26863
oscarnovember <ndukwe.peter@roqqu.com>
2023-05-29 04:14:42 +0100

    create serializable interface

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,2 +1,8 @@
 export type AuthenticatedRequest = Request & any;
 export type CurrencySymbol = 'ngn' | 'usd' | 'eur' | 'gbp'
+
+export interface Serializable {
+  fromJson: (json: string) => any
+  toJson: () => string
+  toString: () => string
+}
